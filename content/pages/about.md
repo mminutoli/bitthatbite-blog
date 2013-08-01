5c2e562028e0f6c47f5734cb5f4811a29c90b808
Marco Minutoli <mminutoli@gmail.com>
2013-08-01 12:14:54 +0200

    Initial import

---
+++ content/pages/about.md
@@ -0,0 +1,13 @@
+Title: About Me
+Date: 2013-06-12 13:42
+Author: Marco Minutoli
+
+Hello all, I’m Marco Minutoli and this is my tech blog.
+
+I am student of computer engineering at Politecnico di Milano mostly
+interested in computer architectures and compilers design and
+implementation.
+
+This blog is for all the things "learnt the hard way". It's for all
+those things I don’t want to forget or I found interesting to share
+with you.
---
+++ pelicanconf.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Marco Minutoli'
+SITENAME = u'Bit that Bite'
+SITEURL = ''
+
+TIMEZONE = u'Europe/Rome'
+# THEME = u'/home/mminutoli/tools/pelican-themes/mnmlist'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  ()
+
+# Social widget
+SOCIAL = (('github', 'https://github.com/mminutoli'),
+          ('Google+', 'https://plus.google.com/114057888676170815651'))
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
