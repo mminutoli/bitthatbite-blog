849151e5272bc6e957590838328ed6bdeba1791a
Marco Minutoli <mminutoli@gmail.com>
2013-09-21 14:43:31 +0200

    Remove relative urls.

--- pelicanconf.py
+++ pelicanconf.py
@@ -28,4 +28,4 @@ SOCIAL = (('github', 'https://github.com/mminutoli'),
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-RELATIVE_URLS = True
+RELATIVE_URLS = False
